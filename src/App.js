1315abe5a5d85bea460e32d35da6d9baa602b427
onyeanuna prince eze <onyeanunaprince@gmail.com>
2021-11-22 16:59:22 +0100

    attempted to fix the white screen issue

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import './App.css';
 /*eslint no-useless-escape: "error"*/
-import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
 import Home from './component/Home';
+import { Switch } from 'react-router';
 import Planets from './component/Planets.jsx';
 import Characters from './component/Characters';
 
 function App()  {
   return (
     <Router>
-      <Switch>
-        <div className="App">
-          <Route exact path="/" component={Home}/>
-          <Route exact path="/Planets" component={Planets}/>
-          <Route exact path="/Characters" component={Characters}/>
-        </div>
+        <Switch>
+          <div className="App">
+            <Route exact path="/" >
+              <Home/>
+            </Route>
+            <Route exact path="/Planets" >
+              <Planets/>
+            </Route>
+            <Route exact path="/Characters">
+              <Characters/>
+            </Route>
+          </div>
       </Switch>
     </Router>
   );
